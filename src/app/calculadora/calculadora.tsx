ac68d50d8825c12b0cb11b3f11d1d244061cb338
Jesús Mariño <jm17.org@gmail.com>
2025-09-17 00:28:36 -0400

    Update calculadora.tsx

--- src/app/calculadora/calculadora.tsx
+++ src/app/calculadora/calculadora.tsx
@@ -20,65 +20,66 @@ export default function Calculadora() {
       <h1>💰 Calculadora de precios</h1>
       <br />
 
-      <label>
-        <input style={{ marginRight: '10px' }}
-          type="checkbox"
-          checked={seleccion.paginaPrincipal}
-          onChange={e => setSeleccion(s => ({ ...s, paginaPrincipal: e.target.checked }))}
-        />
-        Página principal
-      </label>
-      <br /><br />
-
-      <label>
-        <input style={{ marginRight: '10px' }}
-          type="checkbox"
-          checked={seleccion.paginasEstaticas > 0}
-          onChange={e => setSeleccion(s => ({ ...s, paginasEstaticas: e.target.checked ? 1 : 0 }))}
-        />
-        Páginas estáticas
-        {seleccion.paginasEstaticas > 0 && (
-          <input style={{ marginLeft: '10px', textAlign: 'center' }}
-            min={0}
-            max={20}
-            type="number"
-            value={seleccion.paginasEstaticas}
-            onChange={e => setSeleccion(s => ({ ...s, paginasEstaticas: Number(e.target.value) }))}
+        <label>
+          <input style={{ marginLeft: '10px' }}
+            type="checkbox"
+            checked={seleccion.paginaPrincipal}
+            onChange={e => setSeleccion(s => ({ ...s, paginaPrincipal: e.target.checked }))}
           />
-        )}
-      </label>
-      <br /><br />
+          Página principal
+        </label>
+        <br /><br />
 
-      <label>
-        <input style={{ marginRight: '10px' }}
-          type="checkbox"
-          checked={seleccion.paginasDinamicas > 0}
-          onChange={e => setSeleccion(s => ({ ...s, paginasDinamicas: e.target.checked ? 1 : 0 }))}
-        />
-        Páginas Dinámicas
-        {seleccion.paginasDinamicas > 0 && (
-          <input style={{ marginLeft: '10px', textAlign: 'center' }}
-            min={0}
-            max={20}
-            type="number"
-            value={seleccion.paginasDinamicas}
-            onChange={e => setSeleccion(s => ({ ...s, paginasDinamicas: Number(e.target.value) }))}
+        <label>
+          <input style={{ marginRight: '10px' }}
+            type="checkbox"
+            checked={seleccion.paginasEstaticas > 0}
+            onChange={e => setSeleccion(s => ({ ...s, paginasEstaticas: e.target.checked ? 1 : 0 }))}
           />
-        )}
-      </label>
-      <br /><br />
+          Páginas estáticas
+          {seleccion.paginasEstaticas > 0 && (
+            <input style={{ marginLeft: '10px', textAlign: 'center' }}
+              min={0}
+              max={20}
+              type="number"
+              value={seleccion.paginasEstaticas}
+              onChange={e => setSeleccion(s => ({ ...s, paginasEstaticas: Number(e.target.value) }))}
+            />
+          )}
+        </label>
+        <br /><br />
 
-      <label>
-        <input style={{ marginRight: '10px' }}
-          type="checkbox"
-          checked={seleccion.login}
-          onChange={e => setSeleccion(s => ({ ...s, login: e.target.checked }))}
+        <label>
+          <input style={{ marginRight: '10px' }}
+            type="checkbox"
+            checked={seleccion.paginasDinamicas > 0}
+            onChange={e => setSeleccion(s => ({ ...s, paginasDinamicas: e.target.checked ? 1 : 0 }))}
           />
-          Login
-      </label>
-      <br /><br />
+          Páginas Dinámicas
+          {seleccion.paginasDinamicas > 0 && (
+            <input style={{ marginLeft: '10px', textAlign: 'center' }}
+              min={0}
+              max={20}
+              type="number"
+              value={seleccion.paginasDinamicas}
+              onChange={e => setSeleccion(s => ({ ...s, paginasDinamicas: Number(e.target.value) }))}
+            />
+          )}
+        </label>
+        <br /><br />
 
-      <h2>Total: ${total}</h2>
+        <label>
+          <input style={{ marginRight: '10px' }}
+            type="checkbox"
+            checked={seleccion.login}
+            onChange={e => setSeleccion(s => ({ ...s, login: e.target.checked }))}
+            />
+            Login
+        </label>
+        <br /><br />
+
+        <h2>Total: ${total}</h2>
+
     </div>
   )
 }
