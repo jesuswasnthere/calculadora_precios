bd9ff852684153c506d30e4510335296bc075fb8
Jesús Mariño <jm17.org@gmail.com>
2025-09-16 23:04:09 -0400

    (FIX)calculadora
    arreglo de estructura

---
+++ src/app/calculadora/data/precios.ts
@@ -0,0 +1,24 @@
+// data/precios.ts
+export const precios = {
+  implementacion: {
+    paginaPrincipal: 35,
+    paginaEstatica: { min: 3, max: 10 },
+    paginaDinamica: { min: 3, max: 15 }
+  },
+  funcionalidad: {
+    dominio: 12,
+    seccion: { min: 3, max: 10 },
+    animacionElemento: { min: 3, max: 10 },
+    animacionConjunta: { min: 3, max: 20 },
+    login: null, // "Concretar"
+    gestionContenido: null,
+    googleAnalytics: 0,
+    seo: { min: 0, max: 10 },
+    hosting: 0,
+    indexarGoogle: 0
+  },
+  postImplementacion: {
+    cambios: 3,
+    nuevasFuncionalidades: 5
+  }
+}
