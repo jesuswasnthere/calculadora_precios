82b5fe02e388fdebb2406a8e2ca3bed02ba4a1b4
Jesús Mariño <jm17.org@gmail.com>
2026-06-09 17:40:22 -0400

    (FIX)error ts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,23 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Archivo, Montserrat } from "next/font/google";
 import "@/app/styles/globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+// Fuente para Títulos (Gruesa y técnica)
+const fuenteTitulos = Archivo({
+  variable: "--font-titulos",
   subsets: ["latin"],
+  weight: ["700", "900"], 
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+// Fuente para Texto Principal (Limpia y geométrica)
+const fuentePrincipal = Montserrat({
+  variable: "--font-principal",
   subsets: ["latin"],
+  weight: ["400", "600", "700"], 
 });
 
 export const metadata: Metadata = {
-  title: "Calculadora de precios",
+  title: "Calculadora de precios WebPages",
   description: "Saca la cuenta y cotiza tu página web a tu gusto!",
 };
 
@@ -23,8 +27,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+    <html lang="es">
+      {/* Aplicamos ambas variables al body */}
+      <body className={`${fuenteTitulos.variable} ${fuentePrincipal.variable}`}>
         {children}
       </body>
     </html>
